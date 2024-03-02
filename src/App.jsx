4c97163d8d3b76b8df415053f1af242bcc124327
Memo56-FB <memo56_fb@hotmail.com>
2024-03-01 23:28:28 -0600

    🚀 Feat: add blog preview card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import ilustrationArticle from './assets/images/illustration-article.svg'
+import avatar from './assets/images/image-avatar.webp'
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-2xl font-semibold'>Hola de nuevo, por fin me siento como una verga otra vez</h1>
-      <p>Rechazado pero no vencido en la vida perra</p>
-    </>
+    <article className='max-w-96 rounded-[20px] bg-white p-6 grid gap-y-6 font-figtree drop-shadow-inactive transition-all hover:drop-shadow-active'>
+      <img className='rounded-[20px]' src={ilustrationArticle} alt="blog"/>
+      <div className='flex flex-col gap-y-3'>
+        <p className='font-figtree-extrabold text-xs px-3 py-1 bg-yellow rounded-[4px] self-start'>Learning</p>
+        <p className='text-xs'>Published 21 Dec 2023</p>
+        <h3 className='text-xl font-figtree-extrabold'>HTML & CSS foundations</h3>
+        <p className='text-grey'>
+          These languages are the backbone of every website, defining structure, content, and presentation.
+        </p>
+      </div>
+      <div className='flex gap-x-3'>
+        <img className='rounded-full size-8 object-cover' src={avatar} alt="avatar" />
+        <p className='font-figtree-extrabold text-sm'>Greg Hooper</p>
+      </div>
+    </article>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#f4d04e',
+        grey: '#7f7f7f'
+      },
+      fontFamily: {
+        'figtree': ['Figtree', 'sans-serif'],
+        'figtree-italic': ['Figtree italic', 'sans-serif'],
+        'figtree-semibold': ['Figtree semibold', 'sans-serif'],
+        'figtree-extrabold': ['Figtree extrabold', 'sans-serif'],
+      },
+      dropShadow: {
+        'active': '16px 16px #000',
+        'inactive': '8px 8px #000'
+      }
+    },
   },
   plugins: [],
 }
